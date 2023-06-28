82e419942dee4d64692289ca498f571d326958fc
a.kakiris <a.kakiris@noslg.com>
2023-06-28 10:21:42 +0300

    Session in .NET Core

--- Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -49,14 +50,18 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 //shopping cart exists
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
             }
             else {
                 //add card record
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
 
             TempData["success"] = "Cart updated successfully";
-            _unitOfWork.Save();
+            
 
             return RedirectToAction(nameof(Index));
         }
