5d32d06a63061b0f67c5c6c7f6fd8021614b92b6
a.kakiris <a.kakiris@noslg.com>
2023-07-06 18:52:24 +0300

    Display User List

--- Bulky/BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -6,63 +6,23 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = SD.Role_Admin)]
-    public class CompanyController : Controller
+    public class UserController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public CompanyController(IUnitOfWork unitOfWork)
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
         {
-            _unitOfWork = unitOfWork;
+            _db = db;
         }
         public IActionResult Index()
         {
-            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
-            
-            return View(objCompanyList);
-        }
-        public IActionResult Upsert(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                //create
-                return View(new Company());
-            }
-            else
-            {
-                //update
-                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
-                return View(companyObj);
-            }
-            
-        }
-        [HttpPost]
-        public IActionResult Upsert(Company CompanyObj)
-        {
-            if (ModelState.IsValid)
-            {
-                if (CompanyObj.Id == 0)
-                {
-                    _unitOfWork.Company.Add(CompanyObj);
-                }
-                else 
-                {
-                    _unitOfWork.Company.Update(CompanyObj);
-                }
-                _unitOfWork.Save();
-                TempData["success"] = "Company created successfully";
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View(CompanyObj);
-            }
-            
-
+            return View();
         }
 
         #region API CALLS
@@ -70,20 +30,13 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
-            return Json(new { data = objCompanyList });
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            return Json(new { data = objUserList });
         }
         [HttpDelete]
         public IActionResult Delete(int? id)
         {
-            var CompanyToBeDeleted = _unitOfWork.Company.Get(u =>u.Id == id);
-            if (CompanyToBeDeleted == null) 
-            { 
-                return Json(new { success = false, message = "Error while deleting"}); 
-            }
-
-            _unitOfWork.Company.Remove(CompanyToBeDeleted);
-            _unitOfWork.Save();
 
             return Json(new { success = true, message = "Delete Successful" });
         }
