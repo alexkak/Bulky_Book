542d0289ea6846da8106f4abe9e95c1a35f6eca1
a.kakiris <a.kakiris@noslg.com>
2023-07-11 16:17:37 +0300

    Product Image Table

--- Bulky/Bulky.Models/Product.cs
+++ Bulky/Bulky.Models/Product.cs
@@ -40,6 +40,7 @@ namespace BulkyBook.Models
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
-
+        [ValidateNever]
+        public List<ProductImage> ProductImages { get; set; }
     }
 }
